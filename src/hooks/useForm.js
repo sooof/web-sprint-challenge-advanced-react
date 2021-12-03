abf030af302a369756288345412748a627f95935
sooof <sooof@me.com>
2021-12-03 11:16:19 -0800

    useLocalStorage work

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,20 +1,20 @@
 // write your custom hook here to control your checkout form
 import  { useState } from "react";
 
-// export const useForm = initialValue => {
-//     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-//     const [values, setValues] = useStorage(initialValue);
+export const useForm = initialValue => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useStorage(initialValue);
   
-//     const handleChanges = (e) => {
-//         setValues({ ...values, [e.target.name]: e.target.value });
-//       };
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
     
-//       const handleSubmit = (e) => {
-//         e.preventDefault();
-//         setShowSuccessMessage(true);
-//       };
-//     return [values, showSuccessMessage, handleChanges, handleSubmit];
-//   }
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+    return [values, showSuccessMessage, handleChanges, handleSubmit];
+  }
   export const useStorage = (initValue) => {
     const [storedValue, setValue] = useLocalStorage('showSuccess', initValue)
     return [storedValue, setValue] 
